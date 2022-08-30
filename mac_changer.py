26dcd3125926273e75ffa13da96c64aed16a128b
root <mercado.gabriel91@hotmail.com>
2022-08-30 16:25:45 -0500

    Feat@v.01: Added python3 compat

--- mac_changer.py
+++ mac_changer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+
+# MODULES IMPORTS
 import subprocess
 import optparse
+import re
 
 
+# Get arguments and set options
 def getArguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface",
@@ -19,6 +23,7 @@ def getArguments():
     return options
 
 
+# Change MAC address function
 def change_mac(interface, new_mac):
     print("[+] changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
@@ -26,6 +31,26 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+# Get current mac address for the interface
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_addr_result = re.search(
+        r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_addr_result:
+        return mac_addr_result.group(0)
+    else:
+        print("[-] Could not read the MAC address")
+
+
 options = getArguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC: " + str(current_mac))
 
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+
+if current_mac == get_current_mac(options.interface):
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[+] MAC address did NOT get changed")
